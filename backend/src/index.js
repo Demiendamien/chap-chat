65c17b8d6eb403ebe1d724271fb05e3b73e19775
Demiendamien <demiendamien5@gmail.com>
2025-10-22 15:15:05 +0200

    Projet terminer2

--- backend/src/index.js
+++ backend/src/index.js
@@ -92,7 +92,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(_dirname, '/frontend/dist')));
 
   // Remplacer * par /* pour path-to-regexp
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(_dirname, 'frontend', 'dist', 'index.html'));
   });
 }
