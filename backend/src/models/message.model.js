bc41063a043d3f8ed0856f65467146b7257139a7
Demiendamien <demiendamien5@gmail.com>
2025-10-22 19:55:42 +0200

    Projet termin5

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,29 +1,52 @@
 import mongoose from "mongoose";
 
+// const messageSchema = new mongoose.Schema(
+//     {
+//         senderId : {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+//         receiverId : {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+//         text: {
+//             type: String,
+//             required: true,
+//         },
+//         image : {
+//             type: String,
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+
 const messageSchema = new mongoose.Schema(
-    {
-        senderId : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        receiverId : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },  
-        text: {
-            type: String,
-            required: true,
-        },
-        image : {
-            type: String,
-        },
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps: true,
-    }
-); 
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: function () {
+        return !this.image; // texte requis seulement s'il n'y a pas d'image
+      },
+    },
+    image: { type: String, default: null },
+  },
+  { timestamps: true }
+);
 
 const Message = mongoose.model("Message", messageSchema);
 
